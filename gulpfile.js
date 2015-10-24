53b7bcab64019e019db69a1ec37e7cb898c1b93f
deein <deein@deein.local>
2015-10-24 23:33:35 +0200

    Added browserSync with gulp-serve to update automatically your browser

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     notify = require("gulp-notify"),
     copy = require('gulp-copy');
+// BrowserSync
+var browserSync = require('browser-sync').create();
 // Config directories
 var config = {
     publicPath: 'www',
@@ -14,30 +16,27 @@ var config = {
 };
 // Gulp tasks
 gulp.task('bower', function() {
-    return bower()
-    .pipe(gulp.dest(config.bowerDir));
+    return bower().pipe(gulp.dest(config.bowerDir));
 });
 gulp.task('lib', function() {
     return gulp.src([
         config.bowerDir + '/jquery/dist/jquery.js',
         config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.js',
         config.bowerDir + '/requirejs/require.js'
-    ])
-	.pipe(uglify())
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest(config.publicPath + '/js/lib/'));
+    ]).pipe(uglify()).pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.publicPath + '/js/lib/'));
 });
 gulp.task('js', function() {
     return gulp.src([
         config.devPath + '/js/**/*.js'
     ])
-	.pipe(concat('main.min.js'))
-	.pipe(uglify())
-	.pipe(sourcemaps.write('./'))
-	.pipe(gulp.dest(config.publicPath + '/js/'))
-	.on("error", notify.onError(function(error) {
+    .pipe(concat('main.min.js')).pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.publicPath + '/js/'))
+    .on("error", notify.onError(function(error) {
         return "Error: " + error.message;
-    }));
+    }))
+    .pipe(browserSync.stream());
 });
 gulp.task('fonts', function() {
     return gulp.src([
@@ -50,4 +49,12 @@ gulp.task('fonts', function() {
 gulp.task('watch', function() {
     gulp.watch(config.devPath + '/js/**/*.js', ['js']);
 });
-gulp.task('default', ['bower', 'lib', 'js', 'fonts']);
+gulp.task('serve', ['js'], function() {
+    browserSync.init({
+        server: config.publicPath
+    });
+    gulp.watch(config.devPath + '/js/**/*.js', ['js']);
+    gulp.watch(config.publicPath + '/css/**/*.css').on('change', browserSync.reload);
+    gulp.watch(config.publicPath + '/*.html').on('change', browserSync.reload);
+});
+gulp.task('default', ['bower', 'lib', 'js', 'fonts', 'serve']);
